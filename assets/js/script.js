804ed08dd3ddb686ff7df2ccc918b42eb598451b
Hailey Strauss <hstrauss2001@gmail.com>
2023-06-16 14:48:03 -0400

    new stuff

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,5 +1,24 @@
 // Assignment Code
 var generateBtn = document.querySelector("#generate");
+var lowercase = "abcdefghijklmnopqrstuvwxyz";
+var uppercase = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+var numbers = "1234567890";
+var special = '!@#$%^&*()_+{}[]|\\/,./?:"';
+
+function generatePassword() {
+  var pwLengthInput = prompt(
+    "What length do you prefer your password to be? Please choose between 8 and 128 characters."
+  );
+
+  var passwordLength = Number(pwLengthInput);
+
+  if (passwordLength < 8 || passwordLength > 128 || isNaN(passwordLength)) {
+    return alert("wrong");
+  }
+
+  var isLowercase = confirm("Do you want tyour pw to contain lowercase chars");
+  console.log(isLowercase);
+}
 
 // Write password to the #password input
 function writePassword() {
@@ -7,7 +26,6 @@ function writePassword() {
   var passwordText = document.querySelector("#password");
 
   passwordText.value = password;
-
 }
 
 // Add event listener to generate button
